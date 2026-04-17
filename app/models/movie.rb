05f4a5247605888cabb24baac7cb4464a5ab3a72
jules le fur <jules.lefur@outlook.fr>
2026-04-16 21:49:04 -0400

    exercicie fini

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
 validates :title, presence: true, uniqueness: true
 validates :overview, presence: true
-has_many :bookmarks, dependent: :restrict_with_error
+has_many :bookmarks
 end
--- db/migrate/20260416224618_create_bookmarks.rb
+++ db/migrate/20260416224618_create_bookmarks.rb
@@ -1,7 +1,7 @@
 class CreateBookmarks < ActiveRecord::Migration[8.1]
   def change
     create_table :bookmarks do |t|
-      t.references :movie, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: { on_delete: :restrict }
       t.references :list, null: false, foreign_key: true
       t.string :comment
 
